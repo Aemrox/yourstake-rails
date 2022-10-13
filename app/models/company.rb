d13c5a5816a4c17b64c2553c553176f0961bbf53
Adam May <adam.may2164@gmail.com>
2022-10-13 16:56:48 -0400

    add feature where each company tracks the number of problem areas
    Satisfy requirement where each company listed also lists the number of
    problem areas it is exposed to. Add a method on the model, and display
    on the company table via the partial and index page

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,5 @@
 class Company < ApplicationRecord
+  def issues_exposed_to
+    [animal_testing, nuclear_weapons, coal_power, rainforest_destruction].reduce(0) {|issues, field| field ? issues : issues += 1 }
+  end
 end
